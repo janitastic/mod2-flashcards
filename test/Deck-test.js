69b963e340493d4a8ad65981d93676c22612f971
Jani Palomino <palomino.jani@gmail.com>
2021-11-29 19:59:54 -0700

    Add instantiation tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,4 +6,13 @@ const Card = require('../src/Card');
 
 describe.only('Deck', () => {
 
+  it('should be a function', () => {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', () => {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
 });
