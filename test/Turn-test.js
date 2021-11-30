2d1e6b9bd7876d387f33653295646bc9becfda53
Jani Palomino <palomino.jani@gmail.com>
2021-11-29 18:14:08 -0700

    Setup Turn class and constructor

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,5 +4,8 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 
 describe('Turn', () => {
-
+  it('should be a function', () => {
+    const turn = new Turn();
+    expect(turn).to.be.a('function');
+  });
 });
