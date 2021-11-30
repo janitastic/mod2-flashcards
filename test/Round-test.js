d334f6023064db75e3437510e1cc968ecef1827e
Jani Palomino <palomino.jani@gmail.com>
2021-11-29 21:44:13 -0700

    Add Round constructor and instantiation tests

--- test/Round-test.js
+++ test/Round-test.js
@@ -6,6 +6,15 @@ const Card = require('../src/Card');
 // const Deck = require('../src/Deck');
 // const Turn = require('..src/Turn');
 
-describe('Round', () => {
+describe.only('Round', () => {
 
+  it('should be a function', () => {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', () => {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  });
 })
