283f935b527bb03c55ddfb719d13d66569db1053
Jani Palomino <palomino.jani@gmail.com>
2021-11-29 21:33:57 -0700

    Setup round test

---
+++ test/Round-test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+// const Deck = require('../src/Deck');
+// const Turn = require('..src/Turn');
+
+describe.only('Round', () => {
+
+})
