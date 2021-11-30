d1236e4dd65bdbd2389fee547b9d2e857634b0c7
Jani Palomino <palomino.jani@gmail.com>
2021-11-29 18:23:39 -0700

    Add tests for user answer and card object arguments

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -8,4 +8,19 @@ describe('Turn', () => {
     const turn = new Turn();
     expect(turn).to.be.a('function');
   });
+
+  it('should be an instance of a Turn', () => {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it('should take in a string that represents a user’s guess to the question as an argument', () => {
+    const turn = new Turn('object');
+    expect(turn.guess).to.be.a('string');
+  });
+
+  it('should take in a Card object as an argument for the current card in play', () => {
+    const turn = new Turn('object', card);
+    expect(card).to.be.an.instanceof(Card);
+  });
 });
