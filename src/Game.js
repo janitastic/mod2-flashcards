2f120b099242cf3cc62ff6ba9246205f8fbb4f8a
Jani Palomino <palomino.jani@gmail.com>
2021-11-30 22:55:54 -0700

    Add test to keep track of currentRound

--- src/Game.js
+++ src/Game.js
@@ -3,8 +3,18 @@ const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {
-    this.currentRound = null;
+  constructor(round) {
+    this.currentRound = round;
+  }
+
+  start() {
+    const cards = prototypeQuestions.map(card => {
+      return card = new Card(card.id, card.question, card.answers, card.correctAnswer);
+    });
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
   }
 
   printMessage(deck, round) {
--- test/Game-test.js
+++ test/Game-test.js
@@ -2,14 +2,38 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Game = require('../src/Game');
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
 
-describe('Game', () => {
+describe.only('Game', () => {
+  let card1;
+  let card2;
+  let card3;
+  let cards;
+  let deck;
+  let round;
+  let game;
+
+  beforeEach('setup', () => {
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    cards = [card1, card2, card3];
+    deck = new Deck(cards);
+    round = new Round(deck);
+    game = new Game(round);
+  });
+
   it('should be a function', () => {
     expect(Game).to.be.a('function');
   });
 
   it('should be an instance of Game', () => {
-    const game = new Game();
     expect(game).to.be.an.instanceof(Game);
+  });
+
+  it('Should keep track of the currentRound', () => {
+    expect(game.currentRound).to.deep.equal(round);
   });
 });
